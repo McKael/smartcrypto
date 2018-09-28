9bd4309467f7367d12bab0d53017c122a4acf59a
Mikael Berthe <mikael@lilotux.net>
2018-09-28 19:55:30 +0200

    Cleanup C code

--- crypto.c
+++ crypto.c
@@ -8,44 +8,6 @@
 #include "keys.h"
 #include "crypto.h"
 
-/*
-void bufToHex(unsigned char *buf, char *out, int len, int doBig)
-{
-	char tmpBuf[0x10];
-	if(doBig)
-		sprintf(out,"%02X",buf[0]);
-	else
-		sprintf(out,"%02x",buf[0]);
-	for(int i=1; i< len ; i++)
-	{
-		if(doBig)
-			sprintf(tmpBuf,"%02X",buf[i]);
-		else
-			sprintf(tmpBuf,"%02x",buf[i]);
-		strcat(out,tmpBuf);
-	}
-}
-unsigned char *HexToBuf(const char *hex)
-{
-	int hexLen = strlen(hex);
-	unsigned char *bytearray = malloc(hexLen+10);
-	for (int i = 0; i < (hexLen / 2); i++)
-	{
-		sscanf(hex + 2*i, "%02x", (unsigned int*)&bytearray[i]);
-		//printf("bytearray %02x\n", bytearray[i]);
-	}
-	return bytearray;
-}
-void printBuffer(char *label, unsigned char *buf, int bufSize)
-{
-	printf("%s: ", label);
-	for(int i =0; i< bufSize; i++)
-	{
-		printf("%02x",buf[i]);
-	}
-	puts("");
-}
-*/
 int EncryptParameterDataWithAES(unsigned char *pIn, unsigned char *pOut)
 {
 	unsigned int num;
@@ -88,31 +50,14 @@ int generateServerHello(const char *userId, const char *pin,
 	unsigned char hash[SHA_DIGEST_LENGTH];
 	unsigned char swapped[256];
 	unsigned char encrypted[256];
-	//char dataText[2048];
-	//char hashText[256];
 
 	SHA1((unsigned char*)pin, strlen((char*)pin), hash);
 	memcpy(aes_key_out, hash, 16);
 
-	/*
-	bufToHex(hash,(char*)hashText, 16,0);
-	printf("AES key: %s\n",hashText);
-	*/
-
 	memset(iv, 0, 16);
 	AES_128_CBC_Enc(publicKey, encrypted, hash, iv, 128);
 
-	/*
-	bufToHex(encrypted,(char*)hashText, 128,0);
-	printf("AES encrypted: %s\n",hashText);
-	*/
-
 	EncryptParameterDataWithAES(encrypted,swapped);
-
-	/*
-	bufToHex(swapped,(char*)hashText, 128,0);
-	printf("AES swapped: %s\n",hashText);
-	*/
 
 	dataLen=0;
 	memset(data,0,sizeof(data));
@@ -122,17 +67,8 @@ int generateServerHello(const char *userId, const char *pin,
 	dataLen+=strlen(userId);
 	memcpy(data+dataLen,swapped,128);
 	dataLen+=128;
-	/*
-	bufToHex(data,dataText, dataLen,1);
-	//printf("data buffer: %s\n",dataText);
-	*/
 
 	SHA1(data, dataLen, hash);
-	/*
-	bufToHex(hash,(char*)hashText, SHA_DIGEST_LENGTH,0);
-	printf("hash: %s\n",hashText);
-	*/
-
 	memcpy(hash_out, hash, SHA_DIGEST_LENGTH);
 
 	if (16+dataLen > len) {
@@ -151,8 +87,6 @@ int generateServerHello(const char *userId, const char *pin,
 	memset(&hello_out[pos], 0, 5);
 	pos += 5;
 
-	//printf("ServerHello: 01020000000000000000%02lX%s0000000000\n",
-	//       132+strlen(userId), dataText);
         return pos;
 }
 
@@ -170,12 +104,10 @@ int parseClientHello(const char *clientHello, unsigned int clientHelloSize,
 		char *skprime_out, char *ctx_out)
 {
 	unsigned char hash2[SHA_DIGEST_LENGTH], hash3[SHA_DIGEST_LENGTH];
-	// unsigned char dest_hash[SHA_DIGEST_LENGTH];
 	unsigned char SKPrime[SHA_DIGEST_LENGTH+1], SKPrimeHash[SHA_DIGEST_LENGTH];
-	//unsigned char *dest;
 	unsigned char *userId,pEncWBGx[GX_SIZE], pEncGx[GX_SIZE], *finalBuffer;
-	unsigned char iv[0x10],pGx[GX_SIZE],secretBytes[256], secretLen,thirdHashBuf[512];
-	unsigned int *l,/*firstLen,*/userIdLen,/*thirdLen,destLen,*/ flagPos,finalPos;
+	unsigned char iv[0x10], pGx[GX_SIZE], secretBytes[256], secretLen, thirdHashBuf[512];
+	unsigned int *l, userIdLen, flagPos, finalPos;
 	unsigned int gUserIdLen;
 
 	gUserIdLen = strlen(gUserId);
@@ -194,85 +126,38 @@ int parseClientHello(const char *clientHello, unsigned int clientHelloSize,
 		return ERR_SC_BAD_USERID; // User IDs do not match
 	}
 
-	/*
-	destLen = userIdLen + 132 + SHA_DIGEST_LENGTH;
-	dest=malloc(destLen);
-	thirdLen = userIdLen + 132;
-	memcpy(dest,clientHello+11,thirdLen);
-	memcpy(dest+thirdLen,hash,SHA_DIGEST_LENGTH);
-	*/
-
-	/*
-	printf("\ndest: ");
-	for(int i =0; i< destLen; i++)
-	{
-		printf("%02x",dest[i]);
-	}
-	*/
-
 	userId=malloc(userIdLen+1);
 	memcpy(userId,clientHello+USER_ID_POS,userIdLen);
 	userId[userIdLen]=0;
-	//printf("\nuserId: %s\n",userId);
 
 	if (strcmp((const char *)userId, gUserId)) {
 		return ERR_SC_BAD_USERID; // User IDs do not match
 	}
 
 	memcpy(pEncWBGx,clientHello+USER_ID_POS+userIdLen,GX_SIZE);
-	/*
-	printf("\npEncWBGx: ");
-	for(int i =0; i< GX_SIZE; i++)
-	{
-		printf("%02x",pEncWBGx[i]);
-	}
-	*/
-
 	DecryptParameterDataWithAES(pEncWBGx,pEncGx);
-
-	/*
-	printf("\npEncGx: ");
-	for(int i =0; i< GX_SIZE; i++)
-	{
-		printf("%02x",pEncGx[i]);
-	}
-	*/
 
 	memset(iv, 0, 16);
 	AES_128_CBC_Dec(pEncGx, pGx, (const unsigned char*)aesKey, iv, GX_SIZE);
 
-	/*
-	printf("\npGx: ");
-	for(int i =0; i< GX_SIZE; i++)
-	{
-		printf("%02x",pGx[i]);
-	}
-	puts("");
-	*/
-
-	BIGNUM *bn_prime, *bn_pGx, /**bn_publicKey,*/ *bn_privateKey, *bn_secret;
+	BIGNUM *bn_prime, *bn_pGx, *bn_privateKey, *bn_secret;
 	BN_CTX *ctx; /* used internally by the bignum lib */
 
 	ctx = BN_CTX_new();
 	bn_secret = BN_new();
 	bn_prime = BN_bin2bn(prime,sizeof(prime),NULL);
 	bn_pGx= BN_bin2bn(pGx,GX_SIZE,NULL);
-	/*bn_publicKey =*/ BN_bin2bn(publicKey,GX_SIZE,NULL);
+	BN_bin2bn(publicKey,GX_SIZE,NULL);
 	bn_privateKey = BN_bin2bn(privateKey,GX_SIZE,NULL);
 	BN_mod_exp(bn_secret, bn_pGx, bn_privateKey, bn_prime,ctx);
-	//printf("Secret: %s\n",BN_bn2hex(bn_secret));
 	secretLen=BN_bn2bin(bn_secret,secretBytes);
-	//printBuffer("secret",secretBytes,secretLen);
 
 	memcpy(hash2,clientHello+USER_ID_POS+userIdLen+GX_SIZE,SHA_DIGEST_LENGTH);
-	//printBuffer("hash2",hash2,SHA_DIGEST_LENGTH);
 
 	memcpy(thirdHashBuf, userId,strlen((char*)userId));
 	memcpy(thirdHashBuf+strlen((char*)userId), secretBytes, secretLen);
-	//printBuffer("secret2",thirdHashBuf,secretLen+strlen((char*)userId));
 
 	SHA1(thirdHashBuf, secretLen+strlen((char*)userId), hash3);
-	//printBuffer("hash3",hash3,SHA_DIGEST_LENGTH);
 	if (memcmp(hash2,hash3,SHA_DIGEST_LENGTH)) {
 		return ERR_SC_PIN; // Pin error
 	}
@@ -286,11 +171,6 @@ int parseClientHello(const char *clientHello, unsigned int clientHelloSize,
 	if (htonl(*l)) {
 		return ERR_SC_SECOND_FLAG;  // Second flag error
 	}
-
-	/*
-	SHA1(dest, destLen, dest_hash);
-	//printBuffer("dest_hash",dest_hash,SHA_DIGEST_LENGTH);
-	*/
 
 	finalBuffer = malloc(userIdLen+ strlen((char*)userId)+ 384);
 	finalPos=0;
@@ -307,21 +187,11 @@ int parseClientHello(const char *clientHello, unsigned int clientHelloSize,
 
 	SHA1(finalBuffer, finalPos, SKPrime);
 	SKPrime[SHA_DIGEST_LENGTH]=0;
-	//printBuffer("SKPrime",SKPrime,SHA_DIGEST_LENGTH);
 
 	SHA1(SKPrime, SHA_DIGEST_LENGTH+1, SKPrimeHash);
-	//printBuffer("SKPrimeHash",SKPrimeHash,SHA_DIGEST_LENGTH);
 	applySamyGOKeyTransform(SKPrimeHash,SKPrimeHash);
-	//printBuffer("ctx",SKPrimeHash,16);
 
-	//bufToHex(SKPrime,(char*)out, SHA_DIGEST_LENGTH,0);
-	//strncpy(skprime_out, out, SHA_DIGEST_LENGTH*2);
-	//skprime_out[SHA_DIGEST_LENGTH*2] = 0;
 	memcpy(skprime_out, SKPrime, SHA_DIGEST_LENGTH);
-
-	//bufToHex(SKPrimeHash,(char*)out, 16,0);
-	//strncpy(ctx_out, out, 16*2);
-	//ctx_out[16*2] = 0;
 	memcpy(ctx_out, SKPrimeHash, SHA_DIGEST_LENGTH);
 
 	free(userId);
